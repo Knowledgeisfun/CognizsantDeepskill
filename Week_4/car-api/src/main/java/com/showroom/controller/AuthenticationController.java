bf792d5b2f2edf404b5d920db76f245ebae4559e
Sanjay Vinod K <sanjayvinodk@gmail.com>
2026-07-10 01:13:28 +0530

    JWT-handson

--- Week_4/car-api/src/main/java/com/showroom/controller/AuthenticationController.java
+++ Week_4/car-api/src/main/java/com/showroom/controller/AuthenticationController.java
@@ -4,7 +4,6 @@ import com.showroom.security.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
